c4bd3eba03c420e1b8cc087f4f134ae03557d132
Anaïs PERIGNY <anaisperigny31@gmail.com>
2023-08-28 21:30:05 +0200

    test deployement 4

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -12,7 +12,7 @@ export class TodosController {
     constructor(private readonly todosService : TodosService ) {}
 
     @Get("/all")
-    @Render("todos/all")
+    @Render("/app/views/todos/all")
     async getTodos(@Session() session: Record<string, any>) {
         const currentUser: User = session.user;
         const todos = await this.todosService.getAllTodos(currentUser)
@@ -20,7 +20,7 @@ export class TodosController {
     }
 
     @Get('/add')
-    @Render("/todos/addTodos")
+    @Render("/app/views/todos/addTodos")
     getAddTodos(){}
 
     @UseInterceptors(ClassSerializerInterceptor)
@@ -32,7 +32,7 @@ export class TodosController {
     }
 
     @Get("/detail/:id")
-    @Render("/todos/detail")
+    @Render("/app/views/todos/detail")
     async getTodo(@Param("id") id: string, @Res() response : Response) {
         try {
             const todos = await this.todosService.getTodo(id)
@@ -50,7 +50,7 @@ export class TodosController {
     }
 
     @Get("/update/:id")
-    @Render("/todos/update")
+    @Render("/app/views/todos/update")
     async getModifyTodo(@Param("id") id: string, @Res() response : Response) {
         try {
             const todos = await this.todosService.getTodo(id)
